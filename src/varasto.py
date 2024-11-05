6d3da72c6a19ff3381f9f9c41346225b06655671
Katri <katri.laamala@outlook.com>
2024-11-05 09:41:32 +0200

    viikko 2 tehtävä 4

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,5 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0, workflow_testaus=0):
-        self.testaus=workflow_testaus
+    def __init__(self, tilavuus, alku_saldo = 0):
 
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
